d608730fc98d8ea39a8e7fff11f85cfef8cca303
seona-dev <seona0826@gmail.com>
2022-07-05 23:53:24 +0900

    Successfully Tested with Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("출력");
+        printSign();
+    }
+    static void printSign() {
+        System.out.println("PrintSign 사용");
     }
 }
